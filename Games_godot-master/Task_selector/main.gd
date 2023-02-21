8b83d29f4fb74a52bc45f3e6bc9639d754dee59f
david <davidkueng@gmail.com>
2023-02-21 11:40:01 +0100

    task manager adding new tasks

--- Games_godot-master/Task_selector/main.gd
+++ Games_godot-master/Task_selector/main.gd
@@ -1,6 +1,5 @@
-extends Container
+extends CanvasLayer
 
-var button_id = 0
 var task_suggestion = false
 
 func _ready():
@@ -23,67 +22,58 @@ func random_task():
 func _on_choose_task_button_pressed():
 	var chosen_task = random_task()
 	var tasks = populate_tasks()
+	$end_task.visible = true
+	
 	for i in tasks.size():
 		if tasks[i] == chosen_task:
+			print(chosen_task)
 			if task_suggestion == false:
-				var current_task_button = Button.new()
-				var current_task_label = Label.new()
-				var confirm_task_label = Label.new()
-				$current_task.add_child(current_task_label)
-				$current_task.add_child(current_task_button)
-				$current_task.get_child(1).name = "complete_task_button"
-				$current_task.get_child(0).name = "current_task_label"
-				$current_task/complete_task_button.add_child(confirm_task_label)
-				$current_task/complete_task_button.get_child(0).name = "complete_task_label"
-				$current_task/complete_task_button/complete_task_label.text = "click to end task"
-				$current_task/complete_task_button/complete_task_label.set_align(HALIGN_CENTER)
-				$current_task/complete_task_button/complete_task_label.set_valign(VALIGN_CENTER)
-				$current_task/complete_task_button/complete_task_label.rect_size.x = 800
-				$current_task/complete_task_button/complete_task_label.rect_size.y = 20
-				$current_task/current_task_label.set_align(HALIGN_CENTER)
-				$current_task/current_task_label.set_valign(VALIGN_CENTER)
-				$current_task/current_task_label.rect_size.x = 800
-				$current_task/current_task_label.rect_size.y = 20
-				$current_task/current_task_label.text = chosen_task
-				$current_task/complete_task_button.rect_size.x = 800
-				$current_task/complete_task_button.rect_size.y = 20
-				$suggested_task/choose_task_button/Label.text = "Click to get another suggestion"
+				$current_task.text = chosen_task
+				$suggest_task/choose_task_button/Label.text = "Click to get another suggestion"
+				$end_task/end_task_button.connect("pressed", self, "_on_end_task_button_pressed")
 				task_suggestion = true
-				$current_task/complete_task_button.connect("pressed", self, "_on_current_task_button_pressed")
 				break
 			else:
-				$current_task/current_task_label.text = chosen_task
+				$current_task.text = chosen_task
 	
-func _on_current_task_button_pressed():
+func _on_end_task_button_pressed():
 	var completed_task = RichTextLabel.new()
 	
-	$completed_task.add_child(completed_task)
-	$completed_task.get_children()[-1].bbcode_enabled = true
-	$completed_task.get_children()[-1].rect_min_size.y = 20
-	$completed_task.get_children()[-1].append_bbcode("[s]"+$current_task/current_task_label.text+"[/s]")
-	delete_task($current_task/current_task_label.text)
-	$current_task/current_task_label.text = ""
+	$completed_tasks.add_child(completed_task)
+	$completed_tasks.get_children()[-1].bbcode_enabled = true
+	$completed_tasks.get_children()[-1].rect_min_size.y = 20
+	$completed_tasks.get_children()[-1].append_bbcode("[s]"+$current_task.text+"[/s]")
+	delete_task($current_task.text)
+	$current_task.text = ""
+	$end_task.visible = false
 	
 func delete_task(task):
 	var f = File.new()
-	f.open("res://task_selector.txt", File.READ)
+	f.open("res://task_selector.txt", File.READ_WRITE)
 	var tasks = f.get_as_text().split(",")
 	
 	for i in tasks.size():
 		if tasks[i] == task:
 			tasks.remove(i)
-			f.open("res://task_selector.txt", File.WRITE)
-			#tasks.join(",")
-			#f.store_string(tasks)
-			f.store_csv_line(tasks, ",")
+			f.store_string(tasks.join(","))
 			print("tasks ", f)
 			f.close()
 			break
-		
 			
-			
+func _on_add_task_button_pressed():
+	
+	var f = File.new()
+	f.open("res://task_selector.txt", File.READ_WRITE)
+	var tasks = f.get_as_text().split(",")
+	tasks.push_back($add_task/add_task_prompt.text)
+	f.store_string(tasks.join(","))
+	f.close()
+	$add_task/add_task_prompt.text = ""
 			
 # todo: invisible ("styled" buttons so it is not necessary to do that when adding the content via code
 # todo: remove space from last item in task file after first WRITE to File
+# todo: handle duplicates
 	
-	
+
+
+
